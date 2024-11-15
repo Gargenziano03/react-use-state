882f135f5c170e33e2ecb7b8cc14883a1573b888
Gargenziano03 <gargenziano03@gmail.com>
2024-11-15 15:39:10 +0100

    add function handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import AppHeader from "./components/AppHeader"
-import linguaggi from './db/db.js'
+import { useState } from 'react'
+import AppHeader from './components/AppHeader'
+import codes from './db/db.js'
 function App() {
+  const [active, setActive] = useState(0)
 
+  function handleClick(e) {
+    const newActive = Number(e.target.getAtttibute('data-index'))
+  }
 
   return (
     <>
       <AppHeader />
+      <div className="container">
+        <div className="code">
+          {linguaggi.map((item, index) => (
+            <div className="code-item" key={item - id}>
+              <button onClick={handleClick}></button>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   )
 }
